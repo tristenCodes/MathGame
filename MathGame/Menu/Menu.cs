402ce0934ec27a7d6fdfbc9999525e3f88df0b1d
Tristen <79072811+tristenCodes@users.noreply.github.com>
2024-07-24 22:04:03 -0500

    Add menu options and menu class

---
+++ MathGame/Menu/Menu.cs
@@ -0,0 +1,6 @@
+namespace MathGame.Menu;
+
+public class Menu
+{
+    
+}
--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome to the Math Game. What would you like to be quizzed on?");
+        Console.WriteLine("1. Addition");
+        Console.WriteLine("2. Subtraction");
+        Console.WriteLine("3. Multiplication");
+        Console.WriteLine("4. Division");
+        Console.ReadLine();
+
     }
 }
